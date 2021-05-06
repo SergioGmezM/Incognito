984aca36c837d7657ae0ac65d616f9e59b11137a
TheBender10 <sergiogomez9867@gmail.com>
2021-05-06 13:00:32 +0200

    Player movement now works

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float x = playerTransform.position.x + offset.x;
+        float y = playerTransform.position.y + offset.y;
+        float z = playerTransform.position.z + offset.z;
+        transform.position = new Vector3(x, y, z);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,72 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Animator playerAnim;
+    private Rigidbody playerRB;
     private int isWalkingHash;
     private int isRunningHash;
+    [SerializeField] private float speed = 100.0f;
+    [SerializeField] private float rotationSpeed = 0.5f;
+    [SerializeField] private float maxSqrtVelocity = 500.0f;
+    private bool isWalking;
+    private bool isRunning;
+    private Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAnim = GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody>();
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+        direction = Vector3.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isWalking = playerAnim.GetBool(isWalkingHash);
-        bool isRunning = playerAnim.GetBool(isRunningHash);
+        isWalking = playerAnim.GetBool(isWalkingHash);
+        isRunning = playerAnim.GetBool(isRunningHash);
         bool WASDPressed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction = Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            direction = new Vector3(-1, 0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            direction = new Vector3(1, 0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            direction = new Vector3(-1, 0, -1);
+        }
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            direction = new Vector3(1, 0, -1);
+        }
+
         bool runPressed = Input.GetKey(KeyCode.LeftShift);
 
         // Idle to Walking
@@ -61,6 +110,30 @@ public class PlayerController : MonoBehaviour
         {
             playerAnim.SetBool(isWalkingHash, false);
             playerAnim.SetBool(isRunningHash, false);
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        //Rotate smoothly to this target:
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isWalking)
+        {
+            if (playerRB.velocity.sqrMagnitude < maxSqrtVelocity)
+            {
+                playerRB.AddRelativeForce(Vector3.forward * speed);
+            }
+        }
+
+        if (isRunning)
+        {
+            if (playerRB.velocity.sqrMagnitude < (maxSqrtVelocity * 2))
+            {
+                playerRB.AddRelativeForce(Vector3.forward * speed);
+            }
         }
     }
 }
