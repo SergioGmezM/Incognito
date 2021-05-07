99a1c39722b87dade1144f8674bffdd7c3511349
TheBender10 <sergiogomez9867@gmail.com>
2021-05-07 10:09:56 +0200

    Policemen now patrol the scene

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    private const float THRESHOLD = 2.0f;
+
+    public Transform[] patrolPoints;
+
+    private Rigidbody policemanRB;
+    private Animator objectAnim;
+    private int isWalkingHash;
+    private int isRunningHash;
+    [SerializeField] private float speed = 200.0f;
+    [SerializeField] private float maxSqrtVelocity = 600.0f;
+    [SerializeField] private float startWaitTime = 3.0f;
+    private float waitTime;
+    private int nextPoint;
+    private bool pointReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        policemanRB = GetComponent<Rigidbody>();
+        objectAnim = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+
+        objectAnim.SetBool(isWalkingHash, false);
+        objectAnim.SetBool(isRunningHash, false);
+
+        waitTime = startWaitTime;
+        nextPoint = -1;
+        pointReached = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (pointReached)
+        {
+            if (waitTime < 0)
+            {
+                nextPoint++;
+                nextPoint = nextPoint % patrolPoints.Length;
+                waitTime = startWaitTime;
+                pointReached = false;
+
+                transform.LookAt(patrolPoints[nextPoint]);
+
+                // Idle to Running
+                objectAnim.SetBool(isRunningHash, true);
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        } else
+        {
+            if (Vector3.Distance(transform.position, patrolPoints[nextPoint].position) > THRESHOLD)
+            {
+                transform.LookAt(patrolPoints[nextPoint]);
+
+                if (policemanRB.velocity.sqrMagnitude < maxSqrtVelocity)
+                {
+                    policemanRB.AddRelativeForce(Vector3.forward * speed);
+                }
+                else
+                {
+                    policemanRB.velocity /= 2;
+                }
+            }
+            else
+            {
+                // Running to Idle
+                objectAnim.SetBool(isRunningHash, false);
+
+                policemanRB.velocity = Vector3.zero;
+
+                pointReached = true;
+            }
+        }
+    }
+}
